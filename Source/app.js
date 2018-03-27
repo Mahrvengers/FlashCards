f6f7544fc8b31f2ded7346755435ce74355978b9
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-03-27 13:17:26 +0200

    Added the /hello route

--- Source/app.js
+++ Source/app.js
@@ -12,7 +12,11 @@ const app = express();
 let port = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
-    response.send("I luv da mahrvengers.");
+    response.send("<h2>I luv da mahrvengers.</h2>");
+});
+
+app.get("/hello", (request, response) => {
+    response.send("<h2>Hello world.</h2>");
 });
 
 app.listen(port);
