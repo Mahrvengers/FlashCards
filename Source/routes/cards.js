b6dc2edfb20cbce454faee64365cd7a8938d6e0a
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-03-29 13:42:33 +0200

    Load the data

--- Source/routes/cards.js
+++ Source/routes/cards.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+// ==> const data = (require("flashcards.json")).data;
+const { data } = require("../data/flashcards.json");
+const { cards } = data;
+
 router.get("/", (request, response) => {
     response.render("card", {
         prompt: "Who is buried in Grant's tomb?",
